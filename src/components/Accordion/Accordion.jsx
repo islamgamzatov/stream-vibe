e35960457fb107d0afe654b46a9cf574b808cdcc
Islam <gamzatovislam314@gmail.com>
2026-03-26 23:24:03 +0300

    feat(questions): add Accordion component for FAQ section

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,45 @@
+import './Accordion.scss'
+
+const Accordion = (props) => {
+	const {
+		title,
+		id,
+		name, // name нужен, чтобы сгруппировать несколько Accordion в одну группу, чтобы при раскрытии содержимого одного закрывались все другие Accordion из той же группы.
+		isOpen, // Параметр будет отвечать за начальное состояние Accordion
+		children // Внутренне содержимое. То, что раскрывается по клику на заголовок
+	} = props
+
+	return (
+		<div className="accordion">
+			{/*
+				В accordion__details будет выводиться заголовок.
+				В accordion__content будет выводиться по умолчанию скрытый контент.
+			*/}
+			<details 
+				className="accordion__details"
+				name={name}
+				open={isOpen} // Атрибут open у элемента details отвечает за начальное состояние отображения внутреннего содержимого после загрузки страницы.
+			>
+				<summary className="accordion__summary">
+					<h3 className="accordion__title h5">
+						<span role='term' aria-details={id}>{title}</span> {/* этот span нужен для того, чтобы грамотно обеспечить accessibility нашему компоненту аккордеона */}
+					</h3>
+				</summary>
+			</details>
+			<div /* Благодарая указанному id мы свяжем заголовок аккордеона с соотвествующим содержимым */
+				className="accordion__content" id={id}
+				role="definition"
+			>
+				<div className="accordion__content-inner">
+					<div className="accordion__content__body">
+						{children}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+
+
+export default Accordion
--- src/sections/Qusetions/Question.jsx
+++ src/sections/Qusetions/Question.jsx
@@ -1,10 +1,11 @@
 import Section from "../../layouts/Section"
 import Button from "../../components/Button"
 import AccordionGroup from "../../components/AccordionGroup"
+import Accordion from "../../components/Accordion"
 
 const Questions = () => {
 	const questionItems = [ // В значении данной переменной будет массив строк с текстом вопросов, указанных в макете.
-		'What is StreamVibe?',
+		'English, Motherf*cker, do you speak it?',
 		'How much does StreamVibe cost?',
 		'What content is available on StreamVibe?',
 		'How can I watch StreamVibe?',
@@ -24,7 +25,17 @@ const Questions = () => {
 			<AccordionGroup
 				columns={2}
 			>
-				{questionItems}
+				{questionItems.map((question, index) => (
+					<Accordion
+						title={question}
+						id={`question-${index}`} // Так каждому элементу аккордеона сгенерируем уникальный идендификатор (question-0, question-1 и т.д.)
+						name="question"
+						isOpen={index === 0} // Таким образом мы сделаем раскрытым только первый по счету аккордеон из этого списка
+						key={index}
+					>
+						<p>Say what again. SAY WHAT AGAIN! I DARE YOU! I DOUBLE DARE YOU, MOTHERFUCKER! Say what one more god damn time!</p>
+					</Accordion>
+				))}
 			</AccordionGroup>
 		</Section>
 	)
