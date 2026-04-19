4b78532b8b57431f7f63c5971f84a3b9c4270d05
Islam <gamzatovislam314@gmail.com>
2026-04-19 20:47:56 +0300

    feat(support): add base Field component for Support section

---
+++ src/components/Field/Field.jsx
@@ -0,0 +1,54 @@
+import './Field.scss'
+import classNames from 'classnames'
+
+const Field = (props) => {
+	const {
+		className,
+		/*
+			Так мы автоматически преобразуем label в индендификатор,
+			когда компоненту Field в параметр id ничего не передали.
+			Теперь параметр считается как бы не обязательным.
+		*/
+		id = getIdFromTitle(props.label), 
+		label,
+		/*
+			undefined (default) | 'email' | 'text-area'
+		*/
+		type,
+		placeholder,
+		isRequired,
+	} = props
+
+	const Component = type === 'textarea'
+		? 'textarea'
+		: 'input'
+
+	return (
+		<div
+			className={classNames(className, 'field')}
+		>
+			<label 
+				className="field__label"
+				htmlFor={id} // В обычном HTML для label обычно указывают атрибут for, но по правилам JSX, т.к. он работает про правилам JS синтаксиса, слово for является зарезервированным, поэтому вместо него здесь используется параметр htmlFor, который в финальной разметке станет классическим атрибутом for.
+			>
+				{label} {isRequired && (
+					<span 
+						className="field__required-star"
+						aria-hidden={true} // Здесь это нужно, чтобы для пользователей скринридера не зачитывался текст с прибавкой в виде слова "звездочка" в конце.
+					>*</span>
+				)}
+			</label>
+			<div className="field__body"> {/* Зачем, собственно говоря, нужна эта обертка? Есть вариации field__body, внутри которых выводится не только input, но и select*/}
+				<Component
+					className="field__control"
+					id={id}
+					type={type}
+					placeholder={placeholder}
+					required={isRequired}
+				/>
+			</div>
+		</div>
+	)
+}
+
+export default Field
